26e3e3fcf774ba91b116812dd4981c257d791155
burzum <meryemajmani33@gmail.com>
2025-02-14 02:50:20 +0100

    Fixed Month Calculator

--- src/utils.js
+++ src/utils.js
@@ -3,5 +3,5 @@ export function calculateDurationInMonths(expression) {
     const startDate = new Date(start);
     const endDate = end == "Present" ? new Date() : new Date(end);
     const months = (endDate.getFullYear() - startDate.getFullYear()) * 12 + (endDate.getMonth() - startDate.getMonth());
-    return months;
+    return months + 1;
   }
